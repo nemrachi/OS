1daa37141c6d1c4b0a7fe2a7e39b8843194b8087
Ema Richnakova <xrichnakova@stuba.sk>
2022-12-14 14:04:33 +0100

    symlink done

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -333,6 +333,38 @@ sys_open(void)
       end_op();
       return -1;
     }
+    
+    if (ip->type == T_SYMLINK) {
+      if ((omode & O_NOFOLLOW) == 0) {
+        char target[MAXPATH];
+        int recursive_depth = 0;
+
+        while (1) {
+          if (recursive_depth >= 10) {
+            iunlockput(ip);
+            end_op();
+            return -1;
+          }
+
+          if (readi(ip, 0, (uint64)target, ip->size-MAXPATH, MAXPATH) != MAXPATH) {
+            return -1;
+          }
+
+          iunlockput(ip);
+          if ((ip = namei(target)) == 0) {
+            end_op();
+            return -1;
+          }
+
+          ilock(ip);
+          if (ip->type != T_SYMLINK) {
+            break;
+          }
+
+          recursive_depth++;
+        }
+      }
+    }
   }
 
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
@@ -501,5 +533,29 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64 sys_symlink(void) {
+  char target_dir[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if (argstr(0, target_dir, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0) {
+    return -1;
+  }
+
+  begin_op();
+  if ((ip = namei(path)) == 0) {
+    ip = create(path, T_SYMLINK, 0, 0);
+    iunlock(ip);
+  }
+
+  ilock(ip);
+  if (writei(ip, 0, (uint64)target_dir, ip->size, MAXPATH) != MAXPATH) {
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
